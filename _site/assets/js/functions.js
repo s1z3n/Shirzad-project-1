dfbc81b07f4445793ce631d25eda8c89f0f88a9e
Ali Beheshti <alibeheshti@triaset.com>
2017-11-25 15:39:26 +0330

    Content update

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -86,30 +86,30 @@ $(document).ready(function() {
     });
 
 
+  $('.sub-btn1').click(function(e){
+      console.log(e.currentTarget.className)
+      $('#sub-holder1').addClass('sub-open');
+      // return false
+    });
+
+  $('.sub-btn2,.sub-btn3').click(function(e){
+    console.log(e.currentTarget.className)
+    $('#sub-holder1').removeClass('sub-open');
+  });
+
+
+  // $('.link-wrapper').on('click', function(e){
+  //     wrapperName = e.currentTarget.className
+
+  //     console.log($(wrapperName.toString() +' a').className != 'sub-open')
+  //     if ($(wrapperName.toString() +' a').className != 'sub-open') {
+  //       $(wrapperName.toString() +' a').addClass('sub-open')
+  //       $('#sub-holder1').addClass('sub-open');
+  //     } else {
+  //       $(wrapperName.toString() +' a').removeClass('sub-open');
+  //       $('#sub-holder1').removeClass('sub-open');
+  //     }
+  // })
+
 
 });
-
-
-
-$('.link-wrapper').on('click', function(e){      
-  
-    wrapperName = e.currentTarget.className
-    wrapperName = wrapperName.split(' ')
-    var strNum = wrapperName[1].match(/\d+/)
-  
-    $('#sub-holder'+strNum+'')
-    if ($('#sub-holder'+strNum+'').hasClass('sub-open')) {        
-  
-        $('#sub-holder'+strNum+'').removeClass('sub-open');
-  
-    } else {
-      for(var i = 1; i < 16; i++) {
-        if($('#sub-holder'+i+'').hasClass('sub-open')){
-          $('#sub-holder'+i+'').removeClass('sub-open')
-        }
-      }    
-      $('#sub-holder'+strNum+'').addClass('sub-open');
-      
-    }
-    
-  })
