defbe47efd6de6b63f5766d06189907da9ca2af3
Ali Beheshti <alibeheshti@triaset.com>
2017-11-28 17:06:04 +0330

    Secondary menu fix

--- _site/assets/js/functions.js
+++ _site/assets/js/functions.js
@@ -109,18 +109,58 @@ $(document).ready(function() {
       wrapperName = e.currentTarget.className
       wrapperName = wrapperName.split(' ')
       elemNum = wrapperName[1].match(/\d+/)[0]
-      
+
       if ($('#sub-holder'+elemNum+'').hasClass('sub-open')) {
 
-          $('#sub-holder'+elemNum+'').removeClass('sub-open');        
-        
+          $('#sub-holder'+elemNum+'').removeClass('sub-open');
+
       } else {
         for( var i = 1; i < 16; i++) {
           if ($('#sub-holder'+i+'').hasClass('sub-open')) {
-            $('#sub-holder'+i+'').removeClass('sub-open');        
+            $('#sub-holder'+i+'').removeClass('sub-open');
           }
         }
         $('#sub-holder'+elemNum+'').addClass('sub-open')
-        
+
       }
-  }) 
+  })
+
+  $(document).ready(function () {
+      $(document).on("scroll", onScroll);
+
+      //smoothscroll
+      $('a[href^="#"]').on('click', function (e) {
+          e.preventDefault();
+          $(document).off("scroll");
+
+          $('a').each(function () {
+              $(this).removeClass('active');
+          })
+          $(this).addClass('active');
+
+          var target = this.hash,
+              menu = target;
+          $target = $(target);
+          $('html, body').stop().animate({
+              'scrollTop': $target.offset().top+2
+          }, 500, 'swing', function () {
+              window.location.hash = target;
+              $(document).on("scroll", onScroll);
+          });
+      });
+  });
+
+  function onScroll(event){
+      var scrollPos = $(document).scrollTop();
+      $('.menu-center a').each(function () {
+          var currLink = $(this);
+          var refElement = $(currLink.attr("href"));
+          if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+              $('.menu-center a').removeClass("active");
+              currLink.addClass("active");
+          }
+          else{
+              currLink.removeClass("active");
+          }
+      });
+  }
